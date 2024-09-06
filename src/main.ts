9c24db017ea179534e1b43ed0648c9f0f73279cd
Gump <gump.hyt@gmail.com>
2024-09-06 16:43:38 +0800

    初始化element和bootstrap和去掉自带样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// 导入Bootstrap的CSS文件
+import 'bootstrap/dist/css/bootstrap.min.css'
+// 导入Bootstrap的JS文件
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// 引入 Element Plus
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +19,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
