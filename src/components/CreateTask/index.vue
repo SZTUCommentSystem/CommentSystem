ebf003f837dd0eba52757d10c27477b0af6b626e
我发个呆 <12588276+im-in-a-daze@user.noreply.gitee.com>
2024-09-21 18:59:55 +0800

    作业-创建题目初版完成

--- src/components/CreateTask/index.vue
+++ src/components/CreateTask/index.vue
@@ -3,42 +3,249 @@
 
 <template>
     <div>
-        <h2>新建作业</h2>
+        <h2 style="margin-bottom: 10px;">新建作业</h2>
         <div class="create_title">
-            <p>标题：</p><br>
+            <p>标题：</p>
             <input type="text" placeholder="请输入作业标题" />
         </div>
         <div class="create_title">
-            <p>描述：</p><br>
+            <p>描述：</p>
             <input type="text" placeholder="请输入作业简介" />
+        </div>
+        <div class="create_task">
+            <p>作业列表：</p>
+            <div class="task_list">
+                <ul>
+                    <li>
+                        <div class="type1">
+                            <p>题目：</p>
+                            <input type="text" placeholder="请输入作业题目" />
+                        </div>
+                    </li>
+                    <li>
+                        <div class="type1">
+                            <p>背景：</p>
+                            <input type="text" placeholder="请输入作业背景" />
+                        </div>
+                    </li>
+                    <li>
+                        <div class="type2">
+                            <p>制图要求：</p>
+                            <ul>
+                                <li>
+                                    <input type="text">
+                                    <i class="icon-del">x</i>
+                                </li>
+                                <li>
+                                    <input type="text">
+                                    <i class="icon-del">x</i>
+                                </li>
+                            </ul>
+                            <i class="icon-add">+</i>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="type2">
+                            <p>视图方向：</p>
+                            <ul>
+                                <li>
+                                    <input type="text">
+                                    <i class="icon-del">x</i>
+                                </li>
+                            </ul>
+                            <i class="icon-add">+</i>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="type1">
+                            <p>制图标准：</p>
+                            <input type="text" placeholder="请输入制图标准" />
+                        </div>
+                    </li>
+                    <li>
+                        <div class="type2">
+                            <p>技术要求：</p>
+                            <ul>
+                                <li>
+                                    <input type="text">
+                                    <i class="icon-del">x</i>
+                                </li>
+                                <li>
+                                    <input type="text">
+                                    <i class="icon-del">x</i>
+                                </li>
+                            </ul>
+                            <i class="icon-add">+</i>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="type1">
+                            <p>绘图工具和材料：</p>
+                            <input type="text" placeholder="请输入绘图工具和材料" />
+                        </div>
+                    </li>
+                    <li>
+                        <div class="type2">
+                            <p>评分标准（请确保总评分相加为100%）：</p>
+                            <ul>
+                                <li>
+                                    <input type="text">
+                                    <i class="icon-del">x</i>
+                                </li>
+                                <li>
+                                    <input type="text">
+                                    <i class="icon-del">x</i>
+                                </li>
+                                <li>
+                                    <input type="text">
+                                    <i class="icon-del">x</i>
+                                </li>
+                                <li>
+                                    <input type="text">
+                                    <i class="icon-del">x</i>
+                                </li>
+                            </ul>
+                            <i class="icon-add">+</i>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="type1">
+                            <p>注意事项：</p>
+                            <input type="text" placeholder="请输入注意事项" />
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="button_container">
+                <el-button type="primary" style="margin-top: 10px;">添加题目</el-button>
+            </div>
+        </div>
+        <div class="button_submit">
+            <el-button type="primary">提交</el-button>
+            <el-button>取消</el-button>
         </div>
     </div>
 </template>
 
 <style scoped>
-.create_title {
+.create_title,
+.create_task {
     display: flex;
-    align-items: center;
+    /* 纵向排列 */
+    flex-direction: column;
+    /* 向左对齐 */
+    align-items: flex-start;
     width: 63vw;
-    height: 10vh;
-    margin-top: 20px;
-    padding-left: 20px;
+    height: 13vh;
+    margin-bottom: 5px;
+    padding: 10px 20px;
     background-color: #fff;
 
     p {
         margin: 0;
-        font-size: 1.5vw;
+        font-size: 1.2vw;
     }
 
     input {
-        width: 56vw;
+        display: inline-block;
+        width: 100%;
         height: 6vh;
+        margin-top: 5px;
         padding-left: 10px;
         border: 1px solid #ccc;
+        border-radius: 10px;
 
         &:focus {
             outline: none;
         }
+    }
+}
+
+.create_task {
+    height: auto;
+}
+
+.task_list {
+    width: 100%;
+    height: auto;
+    border: 1px solid #ccc;
+    margin-top: 5px;
+    padding: 10px;
+    padding-left: 0;
+    padding-right: 2rem;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+
+    li {
+        margin-top: 10px;
+
+        &:first-child {
+            margin-top: 0;
+        }
+    }
+}
+
+.button_container {
+    display: flex;
+    align-self: flex-end;
+    justify-content: flex-end;
+    width: 100%;
+}
+
+.button_submit {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 20px;
+    margin-right: 15px;
+
+    button {
+        margin-left: 10px;
+        padding: 0 35px;
+    }
+}
+
+.type1,
+.type2 {
+    p {
+        font-weight: 600;
+    }
+}
+
+.type2 {
+
+    ul {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        li {
+            list-style: auto;
+            margin-left: 10px;
+
+            input {
+                height: 4vh;
+                width: 60%;
+            }
+        }
+    }
+
+    .icon-add {
+        display: inline-block;
+        width: 25px;
+        height: 25px;
+        text-align: center;
+        border-radius: 50px;
+        margin-top: 10px;
+        margin-left: 220px;
+        color: #fff;
+        background-color: #409EFF;
+        cursor: pointer;
+    }
+
+    .icon-del {
+        vertical-align: text-bottom;
+        margin-left: 10px;
+        cursor: pointer;
     }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,7 +21,7 @@ import { RouterView } from "vue-router";
 <style scoped>
 .router-view-container {
   padding: 20px 0;
-  background-color: rgb(245, 246, 246);
+  background-color: rgb(245, 246, 246);;
   /* 设置背景色 */
   min-height: calc(100vh - 60px);
   /* 高度减去导航栏的高度，导航栏高度为60px */
